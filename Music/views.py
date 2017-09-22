6b850714626f429054ec8a9a5dad2e8a4eac594f
813117000 <jason.renwick@my.uwi.edu>
2017-09-21 22:23:59 -0400

    Data Parsing

--- Music/views.py
+++ Music/views.py
@@ -1,4 +1,26 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import requests
+import json
+
 
 def index(request):
     return HttpResponse("<h1> this is the music app homepage</h1>")
+
+
+def test(request):
+    return HttpResponse("<h2> my second page </h2>")
+
+
+def profile(request):
+    jsonlist = []
+    req = requests.get('https://api.github.com/users/jasonrenwick')
+    jsonlist.append(json.loads(req.content))
+    parseddata = []
+    userdata = {}
+
+    for data in jsonlist:
+        userdata['name'] = data['name']
+        
+    content = req.text
+    return HttpResponse(content)
